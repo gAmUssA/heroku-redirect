9da55819c65da8a9370ab929dbe1b10e8af8231a
Viktor Gamov <viktor@confluent.io>
2019-03-10 22:29:33 -0400

    Added root url and www url option

--- web.js
+++ web.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var app = express();
 
-var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
+var newBaseURL = process.env.NEW_BASE_URL || 'http://www.example.com';
+var newRootURL = process.env.NEW_ROOT_URL || 'http://example.com';
 var redirectStatus = parseInt(process.env.REDIRECT_STATUS || 302);
 var port = process.env.PORT || 5000;
 
 app.get('/rel', function(request, response) {
   // response.redirect(redirectStatus, newBaseURL + request.url);
   response.redirect(redirectStatus, newBaseURL);
+});
+
+app.get('*', function (request, response) {
+  response.redirect(redirectStatus, newRootURL);
 });
 
 app.listen(port, function() {
